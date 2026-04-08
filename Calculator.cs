248115487b5d2ac6f6d4960f35a139bf60ef5b4e
spurwa <s.purwa@students.amikom.ac.id>
2026-04-08 14:48:55 +0700

    fungsi main

--- Calculator.cs
+++ Calculator.cs
@@ -2,6 +2,13 @@ class Calculator
 {
 	static void Main(string[] args)
 	{
+		int a = 10;
+		int b = 6;
+		
+		Console.WriteLine("hasil penambahan : {0} + {1} = {2}",a,b,Penambahan(a,b));
+		Console.WriteLine("hasil penambahan : {0} + {1} = {2}",a,b,Penambahan(a,b));
+		Console.WriteLine("\n Tekan apapun untuk keluar ");
+		Console.ReadKey();
 		
 	}
 	static int Penambahan(int a, int b)
